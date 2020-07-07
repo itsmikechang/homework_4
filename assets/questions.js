3a4d93bdb3156c86049b60dd2c115ff8b34cc71d
itsmikechang <msaechang1@gmail.com>
2020-07-07 02:54:35 -0700

    started base index, style, and questions js

---
+++ assets/questions.js
@@ -0,0 +1,53 @@
+var questions = [
+  {
+    title: 'Inside which HTML element do we put the JavaScript?'
+    choices: ['<js>', '<javascript>', '<script>', '<scripting>']
+    answer: '<script>'
+  },
+  {
+    title: 'Where is the correct place to insert a JavaScript?'
+    choices: ['The <head> section', 'The <body> section', 
+    'Both the <head> section and the <body> section are correct']
+    answer: 'Both the <head> section and the <body> section are correct'
+  },
+  {
+    title: 'What is the correct syntax for referring to an external script called "xxx.js"?'
+    choices: ['<script href="xxx.js">', '<script name="xxx.js">', '<script src="xxx.js">']
+    answer: '<script src="xxx.js">'
+  },
+  {
+    title: 'How do you write "Hello World" in an alert box?'
+    choices: ['alert("Hello World");', 'alertBox("Hello World");', 'msgBox("Hello World");', 'msg("Hello World");']
+    answer: 'alert("Hello World");'
+  },
+  {
+    title: 'The external JavaScript file must contain the <script> tag.'
+    choices: ['True', 'False']
+    answer: 'False'
+  },
+  {
+    title: 'How do you create a function in JavaScript?'
+    choices: ['function myFunction()', 'function = myFunction()', 'function:myFunction()']
+    answer: 'function myFunction()'
+  },  
+  {
+    title: 'How do you call a function named "myFunction"?'
+    choices: ['myFunction()', 'call myFunction()', 'call function myFunction()']
+    answer: 'myFunction()'
+  },  
+  {
+    title: 'How to write an IF statement in JavaScript?'
+    choices: ['if i = 5', 'if (i == 5)', 'if i = 5 then', 'if i == 5 then']
+    answer: 'if (i == 5)'
+  },  
+  {
+    title: 'How does a WHILE loop start?'
+    choices: ['while (i <= 10)', 'while i = 1 to 10', 'while (i <= 10; i++)']
+    answer: 'while (i <= 10)'
+  },  
+  {
+    title: 'How can you add a comment in a JavaScript?'
+    choices: ['//This is a comment', '<!--This is a comment-->', 'this is a comment']
+    answer: '//This is a comment'
+  },
+];
